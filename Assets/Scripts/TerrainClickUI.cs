9be3a6bad0b0eeeadab06156652f2ee46a99fcfc
dandeliondino <68911895+dandeliondino@users.noreply.github.com>
2022-05-31 12:19:59 -0700

    Show on overlay at world -> screen point

--- Assets/Scripts/TerrainClickUI.cs
+++ Assets/Scripts/TerrainClickUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TerrainClickUI : MonoBehaviour
 {
     public GameObject canvas;
+    public GameObject panel;
 
     private void Start()
     {
@@ -18,8 +19,19 @@ public class TerrainClickUI : MonoBehaviour
 
     public void ShowAtTile(GameObject tile)
     {
-        transform.position = tile.transform.position;
+        //Vector3 cameraPosition = Camera.main.WorldToScreenPoint(tile.transform.position);
+        //transform.position = tile.transform.position;
+        panel.transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, tile.transform.position);
         canvas.SetActive(true);
+        //RotateToCamera(Camera.main);
+    }
+
+    public void RotateToCamera(Camera camera)
+    {
+        //transform.rotation = Quaternion.LookRotation(transform.position - camera.transform.position);
+        //transform.LookAt(camera.transform);
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, Vector3.up);
+        
     }
 
 }
